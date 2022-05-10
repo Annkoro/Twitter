05296b9caebd64cfeb01801032f95deed65a4b25
Annkoro <0vk325551w33k8n@gmail.com>
2022-05-10 14:46:23 +0000

    [Add]user&tweet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_09_114142) do
+ActiveRecord::Schema.define(version: 2022_05_10_143624) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
