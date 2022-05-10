d547a7ffab655fe40cac114837b0fb7d3e3daa98
Annkoro <0vk325551w33k8n@gmail.com>
2022-05-10 15:00:44 +0000

    [Update]user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,32 @@
 class UsersController < ApplicationController
   def index
+    @user = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_update_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+    redirect_to root_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :caption)
   end
 end
