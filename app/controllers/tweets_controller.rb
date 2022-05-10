c58d47a5fd3b44eae168cb663b356f1eecd012cc
Annkoro <0vk325551w33k8n@gmail.com>
2022-05-10 14:50:38 +0000

    [Update]tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,19 +1,28 @@
 class TweetsController < ApplicationController
   def index
-  end
-
-  def show
+    @tweet = Tweet.all
   end
 
   def new
+    @user = User.find(params[:user_id])
   end
 
   def create
-  end
-
-  def edit
+    @user = User.find(params[:user_id])
+    @user.tweets.create(tweet_params)
+    redirect_to tweets_index_path
   end
 
   def destroy
+    @tweet = Tweet.find(params[:id])
+    if @tweet.destroy
+      redirect_to "/",flash: {danger: "投稿を削除しました"}
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
   end
 end
